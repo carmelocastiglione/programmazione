f2e69f70357c31aaebfed296b757143f88ec2525
Carmelo Castiglione <carmelo.c.castiglione@gmail.com>
2024-09-16 20:19:43 +0200

    [java] eccezioni

---
+++ java/08_eccezione/_01_Eccezione.java
@@ -0,0 +1,13 @@
+public class _01_Eccezione {
+    public static void main(String[] args) {
+        int numeratore = 10;
+        int denominatore = 0;
+
+        try {
+            int risultato = numeratore / denominatore;
+            System.out.println(risultato);
+        } catch (Exception e) {
+            System.err.println("Divisione per zero");
+        }
+    }
+}
