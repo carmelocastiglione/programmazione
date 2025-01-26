25ec6befeb8ba6fa56c31a84173097f6d5a11811
Carmelo Castiglione <carmelo.c.castiglione@gmail.com>
2025-01-26 18:57:02 +0100

    [python] operatori

---
+++ python/04_operatori/01_matematici.py
@@ -0,0 +1,32 @@
+# Variables
+a = 15
+b = 4
+
+# Addition
+print("Addition:", a + b)  
+
+# Subtraction
+print("Subtraction:", a - b) 
+
+# Multiplication
+print("Multiplication:", a * b)  
+
+# Division
+print("Division:", a / b) 
+
+# Floor Division
+print("Floor Division:", a // b)  
+
+# Modulus
+print("Modulus:", a % b) 
+
+# Exponentiation
+print("Exponentiation:", a ** b)
+
+# Absolute Value (abs)
+res = abs(-10) # Output: 10 (absolute value of -10)
+print(res)
+
+# Round (round)
+res = round(3.14159, 2)  # Output: 3.14 (rounds to 2 decimal places)
+print(res)
---
+++ python/04_operatori/04_assegnazioni.py
@@ -0,0 +1,9 @@
+a = 10
+b = a
+print(b)
+b += a
+print(b)
+b -= a
+print(b)
+b *= a
+print(b)
