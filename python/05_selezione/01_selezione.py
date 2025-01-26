2838023258346f792e58ed85f06a4e130fdacade
Carmelo Castiglione <carmelo.c.castiglione@gmail.com>
2025-01-26 19:04:19 +0100

    [java] selezione

---
+++ python/05_selezione/01_selezione.py
@@ -0,0 +1,38 @@
+age = 20
+if age >= 18:
+    print("Eligible to vote.")
+
+age = 10
+if age <= 12:
+    print("Travel for free.")
+else:
+    print("Pay for ticket.")
+
+age = 25
+if age <= 12:
+    print("Child.")
+elif age <= 19:
+    print("Teenager.")
+elif age <= 35:
+    print("Young adult.")
+else:
+    print("Adult.")
+
+age = 70
+is_member = True
+if age >= 60:
+    if is_member:
+        print("30% senior discount!")
+    else:
+        print("20% senior discount.")
+else:
+    print("Not eligible for a senior discount.")
+
+number = 2
+match number:
+    case 1:
+        print("One")
+    case 2 | 3:
+        print("Two or Three")
+    case _:
+        print("Other number")
