50729e6c87c30fd149472a40245248df57040e1e
Carmelo Castiglione <carmelo.c.castiglione@gmail.com>
2025-09-25 19:03:23 +0200

    [sql] vincoli

--- sql/01_creazione/03_creare_tabelle.sql
+++ sql/01_creazione/03_creare_tabelle.sql
@@ -1,8 +1,27 @@
 CREATE TABLE giocatori (
-    id INT PRIMARY KEY,
-    nome VARCHAR(50) NOT NULL,
-    cognome VARCHAR(50) NOT NULL,
-    goal INT CHECK (goal >= 0),
+    id INT,
+    nome VARCHAR(50),
+    cognome VARCHAR(50),
+    goal INT,
     squadra VARCHAR(50),
-    attivo BOOLEAN NOT NULL DEFAULT TRUE
+    sigla_squadra CHAR(3),
+    peso FLOAT,
+    attivo BOOLEAN,
+    data_nascita DATE,
+    ora_ultima_partita TIME,
+    data_ora_ultima_partita DATETIME,
+    data_ora_inserimento TIMESTAMP
 );
+
+/*
+-- DATA TYPE REFERENCE --
+INT: Integer numbers
+FLOAT: Floating-point numbers
+CHAR(n): Fixed-length string with a length of n characters
+VARCHAR(n): Variable-length string with a maximum length of n characters
+BOOLEAN: True or False values
+DATE: Date values in the format 'YYYY-MM-DD'
+TIME: Time values in the format 'HH:MM:SS'
+DATETIME: Combined date and time values in the format 'YYYY-MM-DD HH:MM:SS'
+TIMESTAMP: Timestamp values, typically used for recording the date and time of an event
+*/
---
+++ sql/01_creazione/04_vincoli.sql
@@ -0,0 +1,32 @@
+CREATE TABLE squadre (
+    id INT PRIMARY KEY,
+    sigla_squadra CHAR(3) NOT NULL UNIQUE,
+    nome VARCHAR(50) NOT NULL,
+    citta VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE giocatori (
+    id INT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    cognome VARCHAR(50) NOT NULL,
+    goal INT CHECK (goal >= 0),
+    squadra VARCHAR(50),
+    peso FLOAT,
+    attivo BOOLEAN NOT NULL DEFAULT TRUE,
+    data_nascita DATE,
+    ora_ultima_partita TIME,
+    data_ora_ultima_partita DATETIME,
+    data_ora_inserimento TIMESTAMP,
+    id_squadra INT,
+    FOREIGN KEY (id_squadra) REFERENCES squadre(id)
+);
+
+/* 
+-- CONSTRAINTS REFERENCE --
+NOT NULL: Ensures that a column cannot have a NULL value
+UNIQUE: Ensures that all values in a column are unique
+PRIMARY KEY: Uniquely identifies each record in a table. A combination of NOT NULL and UNIQUE
+CHECK: Ensures that all values in a column satisfy a specific condition
+DEFAULT: Sets a default value for a column when no value is specified
+FOREIGN KEY: Ensures referential integrity by linking to a primary key in another table
+*/
