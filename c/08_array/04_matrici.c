72754c77f35dbd897e6a3c87d4756be13ce506fc
Carmelo Castiglione <carmelo.c.castiglione@gmail.com>
2026-01-08 10:38:38 +0100

    Add C program to declare, initialize, and print a 3x3 matrix

---
+++ c/08_array/04_matrici.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // Dichiarazione e inizializzazione di una matrice 3x3
+    int matrix[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    // Stampa della matrice
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
